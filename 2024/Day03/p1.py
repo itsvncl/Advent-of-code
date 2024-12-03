2baf02ed13bfba6752620a2590d842e239cb854e
itsvncl <its.vencel@gmail.com>
2024-12-03 08:16:39 +0100

    init day03

---
+++ 2024/Day03/p1.py
@@ -0,0 +1,10 @@
+from utils import load_input, time_solution, InputType
+
+
+def solve(input_data: str) -> str:
+    return
+
+if __name__ == "__main__":
+    input_data = load_input(3, InputType.MAIN)
+    result = time_solution(solve, input_data)
+    print(f"Result: {result}")
