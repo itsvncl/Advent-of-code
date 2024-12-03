5a088f6f567b6dc7576d491aadff5e1dbda6612c
itsvncl <its.vencel@gmail.com>
2024-12-03 10:54:52 +0100

    solve day3 p1 and p2

--- 2024/Day03/p1.py
+++ 2024/Day03/p1.py
@@ -1,8 +1,12 @@
 from utils import load_input, time_solution, InputType
+import re
 
+def prod(list) -> int:
+    return int(list[0]) * int(list[1])
 
 def solve(input_data: str) -> str:
-    return
+    valid_ops = re.findall(r'mul\(\d+,\d+\)', input_data)
+    return sum(map(lambda op : prod(re.findall(r'\d+', op)), valid_ops))
 
 if __name__ == "__main__":
     input_data = load_input(3, InputType.MAIN)
--- 2024/Day03/p2.py
+++ 2024/Day03/p2.py
@@ -1,7 +1,23 @@
 from utils import load_input, time_solution, InputType
+import re
+
+DO = "do()"
+MUL_SIG = "m"
+
+def prod(list) -> int:
+    return int(list[0]) * int(list[1])
 
 def solve(input_data: str) -> str:
-    return
+    valid_ops = re.findall(r'mul\(\d+,\d+\)|do\(\)|don\'t\(\)', input_data)
+    
+    sum = 0
+    execute_enabled = True
+    for op in valid_ops:
+        if op[0] == MUL_SIG and execute_enabled:
+            sum += prod(re.findall(r'\d+', op))
+            continue  
+        execute_enabled = op == DO
+    return sum
 
 if __name__ == "__main__":
     input_data = load_input(3, InputType.MAIN)
