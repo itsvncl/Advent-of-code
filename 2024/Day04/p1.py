3766c44aa1f351b851c1f79e3e6b75d990ff1734
itsvncl <its.vencel@gmail.com>
2024-12-04 10:06:40 +0100

    solve day04 p1 and p2

--- 2024/Day04/p1.py
+++ 2024/Day04/p1.py
@@ -1,7 +1,34 @@
 from utils import load_input, time_solution, InputType
+from typing import List
+
+WORD = "XMAS"
+
+def is_word_in_direction(grid: List[str], grid_i: int, grid_j: int, incr_i: int, incr_j: int, word: str, letter_ind: int = 0) -> bool:    
+    if(grid_i >= len(grid) or grid_i < 0 or grid_j >= len(grid[grid_i]) or grid_j < 0):
+        return False
+    
+    if grid[grid_i][grid_j] == word[letter_ind]:
+        return True if letter_ind + 1 >= len(word) else is_word_in_direction(grid, grid_i + incr_i, grid_j + incr_j, incr_i, incr_j, word, letter_ind + 1)
+
+    return False
+    
+def find_words_in_grid(grid: List[str], word: str) -> int:
+    word_count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == word[0]:
+                word_count += is_word_in_direction(grid, i + 1, j, 1, 0, word, 1)
+                word_count += is_word_in_direction(grid, i - 1, j, -1, 0, word, 1)
+                word_count += is_word_in_direction(grid, i, j - 1, 0, -1, word, 1)
+                word_count += is_word_in_direction(grid, i, j + 1, 0, 1, word, 1)
+                word_count += is_word_in_direction(grid, i + 1, j + 1, 1, 1, word, 1)
+                word_count += is_word_in_direction(grid, i - 1, j - 1, -1, -1, word, 1)
+                word_count += is_word_in_direction(grid, i - 1, j + 1, -1, 1, word, 1)
+                word_count += is_word_in_direction(grid, i + 1, j - 1, 1, -1, word, 1)
+    return word_count
 
 def solve(input_data: str) -> str:
-    return ""
+    return find_words_in_grid(input_data.splitlines(), WORD)
 
 if __name__ == "__main__":
     input_data = load_input(4, InputType.MAIN)
--- 2024/Day04/p2.py
+++ 2024/Day04/p2.py
@@ -1,7 +1,17 @@
 from utils import load_input, time_solution, InputType
 
+VALID_CHARS = "MS"
+
+def is_valid_mas(letter1, letter2):
+    return letter1 != letter2 and letter1 in VALID_CHARS and letter2 in VALID_CHARS
+
 def solve(input_data: str) -> str:
-    return ""
+    x_count = 0
+    grid = input_data.splitlines()
+    for i in range(1,len(grid)-1):
+        for j in range(1,len(grid[i])-1):
+            x_count += grid[i][j] == 'A' and is_valid_mas(grid[i-1][j-1], grid[i+1][j+1]) and is_valid_mas(grid[i+1][j-1], grid[i-1][j+1])
+    return x_count
 
 if __name__ == "__main__":
     input_data = load_input(4, InputType.MAIN)
