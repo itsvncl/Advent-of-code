c22f3ad195da30a36411d6dcd14b7a7bc4f500eb
itsvncl <its.vencel@gmail.com>
2024-12-06 08:49:52 +0100

    init day06 and fix template

--- 2024/Day06/p2.py
+++ 2024/Day06/p2.py
@@ -1,9 +1,9 @@
 from utils import load_input, time_solution, InputType
 
-def solve(input_data: str) -> str:
+def solve(input_data: str) -> str:    
     return ""
              
 if __name__ == "__main__":
-    input_data = load_input(1, InputType.MAIN)
+    input_data = load_input(6, InputType.MAIN)
     result = time_solution(solve, input_data)
     print(f"Result: {result}")
