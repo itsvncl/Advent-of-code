33b824370851e82c0836b445460c5297f3d33860
itsvncl <its.vencel@gmail.com>
2024-12-09 09:14:04 +0100

    solve day09 p1

---
+++ 2024/Day09/p1.py
@@ -0,0 +1,55 @@
+from utils import load_input, time_solution, InputType
+from typing import List, Tuple
+
+#This solution is highly unoptimised :(
+
+def read_disk(input_data:str) -> Tuple[List[str], List[str], List[int]]:
+    free_space_groups = []
+    file_blocks = []
+    file_groups = []
+
+    file_id = 0
+    space_index = 0
+    is_file = True
+    for char in input_data:
+        block_size = int(char)
+        if is_file:
+            file_block = block_size * [str(file_id)]
+            file_blocks.extend(file_block)
+            file_groups.append(file_block)
+            file_id += 1
+        else:
+            free_space_groups.append(range(space_index, space_index + block_size))
+
+        is_file = not is_file
+        space_index += block_size
+    
+    return (file_blocks, file_groups, free_space_groups)
+
+def solve(input_data: str) -> str:
+    file_blocks, file_groups, free_space_groups = read_disk(input_data)
+    allocated_space_size = len(file_blocks)
+    disk = []
+
+    is_file = True
+    while len(disk) < allocated_space_size:
+        if is_file:
+            disk.extend(file_groups.pop(0))
+        else:
+            for i in free_space_groups.pop(0):
+                disk.append(file_blocks.pop())
+        
+        is_file = not is_file
+    
+    disk = disk[:allocated_space_size]
+
+    sum = 0
+    for i in range(allocated_space_size):
+        sum += i * int(disk[i])
+
+    return sum
+             
+if __name__ == "__main__":
+    input_data = load_input(9, InputType.MAIN)
+    result = time_solution(solve, input_data)
+    print(f"Result: {result}")
